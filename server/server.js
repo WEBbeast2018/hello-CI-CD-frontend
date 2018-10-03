08db3ae76714ab30353150dec940f3251e1fd94a
yuvalbl <yuval.bar.levi@gmail.com>
2018-10-03 13:57:41 +0300

    02_adding_server

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+const proxyConfig = JSON.parse(fs.readFileSync('../config/production.proxy.json'));
+const proxyUrl = proxyConfig['/quote'].target + '/quote';
+const axios = require('axios');
+
+
+// log middleware
+app.use(function (req, res, next) {
+  console.log(`request url: ${req.url}`);
+  next()
+});
+
+// static files
+app.use(express.static(path.join(__dirname, '../dist/hello-ci-cd-frontend')));
+
+// backend API
+app.use('/quote', (req, res) => {
+  axios.get(proxyUrl)
+    .then(backendResponse => res.send(backendResponse.data));
+});
+
+// index file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/hello-ci-cd-frontend/index.html'));
+});
+
+
+const PORT = process.env.PORT || 3001;	//process.env.PORT is used by heroku
+app.listen(PORT,
+  () => console.log(`server listening on port ${PORT}`)
+);
